33dda1504c42a392a23c6ebddd2803fc57362a1f
Elayve <lilian.delouvy@gmail.com>
2019-10-04 11:25:56 +0200

    Création de la classe Tirage permettant le tirage aléatoire des nombres de loto

--- Projet_DEVA_L3/src/Tirage.java
+++ Projet_DEVA_L3/src/Tirage.java
@@ -5,7 +5,7 @@ public class Tirage {
 
     private int nbJoueurs;
     private int nextToken;  //prochain numero tiré (entre 1 et 90)
-    private ArrayList<Integer> AlreadyTaken = new ArrayList<Integer>();
+    private ArrayList<Integer> unusedTokens = new ArrayList<Integer>();   //liste des jetons qui n'ont pas encore été utilisés
 
     public int getNbJoueurs() {
         return nbJoueurs;
@@ -26,22 +26,34 @@ public class Tirage {
     public Tirage(int nbJoueurs, int nextToken) {
         this.nbJoueurs = nbJoueurs;
         this.nextToken = nextToken;
+        for(int i=1;i<91;i++){
+            unusedTokens.add(i-1,i);
+        }
     }
 
-    public Tirage(int nbJoueurs, int nextToken, ArrayList<Integer> alreadyTaken) {
+    public Tirage(int nbJoueurs, int nextToken, ArrayList<Integer> unusedTokens) {
         this.nbJoueurs = nbJoueurs;
         this.nextToken = nextToken;
-        AlreadyTaken = alreadyTaken;
+        this.unusedTokens = unusedTokens;
+    }
+
+    public void initGrille(){   //3 lignes de 5 numéros, 9 colonnes de 15 numéros
+
     }
 
     public void RandomToken(){
         Random randomVar=new Random();
-        this.nextToken=randomVar.nextInt(90)+1;   //nombre aléatoire >0 et <91
-        while(AlreadyTaken.contains((Integer)nextToken)){
-
-        }
+        int index=randomVar.nextInt(unusedTokens.size());   //nombre aléatoire >=0 et <taille de la liste des tokens restants
+        this.nextToken=unusedTokens.indexOf(index);     //on récupère le nombre de la liste
+        unusedTokens.remove(index);     //on supprime cet élément de la liste
     }
 
-
-
+    public int getTokenToGame(){
+        if(!unusedTokens.isEmpty()) {
+            RandomToken();
+            return nextToken;
+        }
+        else
+            return 0;       //si la fonction renvoie 0, c'est que la partie est finie, car il n'y a plus de jetons
+    }
 }
