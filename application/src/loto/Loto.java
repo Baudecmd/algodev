033a0d49e1b3882a079c98096399bd61e804df7d
Joffrey-sama <joffreyd59@gmail.com>
2019-10-14 16:04:17 +0200

    Corrections warnings

--- application/src/loto/Loto.java
+++ application/src/loto/Loto.java
@@ -15,12 +15,10 @@ public class Loto implements Partie {
     private List<Integer> tiree;
 
     public Loto(List<Joueur> L) {
-        Joueur temp;
-        this.joueurs = new HashMap();
-        for (int i = 0; i < L.size(); i++){
-            temp = L.get(i);
-            joueurs.put(temp, new Grille(3,9));
-            //joueurs.get(temp).initGrid();
+        this.joueurs = new HashMap<>();
+        for (Joueur j: L){
+            joueurs.put(j, new Grille(3,9));
+            joueurs.get(j).initGrid();
         }
         this.tiree = new ArrayList<>();
         this.tirage = new Tirage(L.size(), 0);
@@ -46,25 +44,18 @@ public class Loto implements Partie {
         tiree.add(tirage.getTokenToGame());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Test de la gestion des scores
         Joueur j1 = new Joueur("Joffrey");
         Joueur j2 = new Joueur("Gaby");
         Joueur j3 = new Joueur("Romane");
         Joueur j4 = new Joueur("Lilian");
         Joueur j5 = new Joueur("Conrad");
-        ArrayList<Joueur> js = new ArrayList<>();
 
         j1.setScore(1);
         j2.setScore(0);
         j3.setScore(2);
         j4.setScore(0);
         j5.setScore(0);
-        js.add(j1);
-        js.add(j2);
-        js.add(j3);
-        js.add(j4);
-        js.add(j5);
-        Loto L1 = new Loto(js);
 
         Partie.ajouterVictoire("resources/scoreboardLoto.ser",j1);
         Partie.ajouterVictoire("resources/scoreboardLoto.ser",j2);
