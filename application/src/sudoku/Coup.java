f9b9d1a8903aa1bf36bb94cfebbcc2fdfde0b56a
Rominou <merveauxr@outlook.fr>
2019-11-01 16:18:31 +0100

    Creation des classes JoueurSudoku/coup, ajout de template de sudoku (sans solution pour l'instant) + vérification pour le coup en cours minimain test

---
+++ application/src/sudoku/Coup.java
@@ -0,0 +1,44 @@
+package sudoku;
+
+public class Coup {
+	private int x;
+	private int y;
+	private int valeurRentree;
+	private static int num = 0 ;
+	
+	public Coup() {
+		super();
+		this.x = -1;
+		this.y = -1;
+		this.valeurRentree = 0;
+		coupSuivant();
+	}
+
+	public void coupSuivant() {
+		Coup.num++;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getValeurRentree() {
+		return valeurRentree;
+	}
+	public void setValeurRentree(int val) {
+		this.valeurRentree = val;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+
+}
