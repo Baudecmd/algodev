53326d026554ae217a3146ca5cf32d46116be5bc
Gabriel Loiseau <loiseaulg@gmail.com>
2019-11-08 00:46:31 +0100

    Ajout début fonctionnalités Bataille navale par Lilian

---
+++ application/bataille/AffichageBN.java
@@ -0,0 +1,20 @@
+package bataille;
+
+import commun.Joueur;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AffichageBN extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ application/bataille/Bataille.java
@@ -0,0 +1,121 @@
+package bataille;
+
+import commun.Joueur;
+import commun.Partie;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Bataille implements Partie {
+
+    JoueurBataille2 j1,j2;
+    boolean firstTurn=true;
+
+    public JoueurBataille2 getJ1() {
+        return j1;
+    }
+
+    public void setJ1(JoueurBataille2 j1) {
+        this.j1 = j1;
+    }
+
+    public JoueurBataille2 getJ2() {
+        return j2;
+    }
+
+    public void setJ2(JoueurBataille2 j2) {
+        this.j2 = j2;
+    }
+
+    public boolean isFirstTurn() {
+        return firstTurn;
+    }
+
+    public void setFirstTurn(boolean firstTurn) {
+        this.firstTurn = firstTurn;
+    }
+
+    public Bataille(JoueurBataille2 j1, JoueurBataille2 j2) {
+        this.j1 = j1;
+        this.j2 = j2;
+    }
+
+    public JoueurBataille2 retournerGagnant(){
+        if(j1.getListeBateaux().isEmpty())  //si la liste des bateaux du joueur 1 est vide...
+            return j2;    //...c'est l'autre joueur qui gagne
+        return j1;
+    }
+
+    public Boolean partieFinie(){
+        if(!j1.getListeBateaux().isEmpty() && !j2.getListeBateaux().isEmpty())      //un bateau détruit est retiré de la liste des bateaux du joueur
+            return false;
+        return true;
+    }
+
+    public void tourSuivant(){
+        int i=-1,j=-1;
+        if(firstTurn){
+            j1.placement();
+            j2.placement();
+            firstTurn=false;
+        }
+        else{
+            //récupération des coordonnées du tir contre le j2 via l'interface
+            j2.tir(i, j);
+            //récupération des coordonnées du tir contre le j1 via l'interface
+            j1.tir(i, j);
+        }
+    }
+
+    // *** Tests *** //
+
+    public void tourSuivantTest(JoueurBataille2 j3,JoueurBataille2 j4){
+        int i,j;
+        Scanner sc=new Scanner(System.in);
+        if(firstTurn){
+            System.out.println(j3.getNom() +", veuillez entrer les coordonnées i et j de l'origine de votre bateau");
+            i=sc.nextInt();
+            j=sc.nextInt();
+            j3.placementTest(i,j);
+            System.out.println(j4.getNom() +", veuillez entrer les coordonnées i et j de l'origine de votre bateau");
+            i=sc.nextInt();
+            j=sc.nextInt();
+            j4.placementTest(i,j);
+            firstTurn=false;
+        }
+        else{
+            //récupération des coordonnées du tir contre le j2 via l'interface
+            System.out.println(j3.getNom() + ", où souhaitez-vous tirer?");
+            i=sc.nextInt();
+            j=sc.nextInt();
+            j4.tir(i, j);
+            if(this.partieFinie())  //la partie peut s'arrêter à la fin du tour du j1. Dans ce cas, pas besoin de demander au j2 de jouer
+                return;
+            //récupération des coordonnées du tir contre le j1 via l'interface
+            System.out.println(j4.getNom() + ", où souhaitez-vous tirer?");
+            i=sc.nextInt();
+            j=sc.nextInt();
+            j3.tir(i, j);
+        }
+    }
+
+    public void afficheGagnant(JoueurBataille2 joueur){
+        System.out.println("Le gagnant est:" + joueur.getNom());
+    }
+
+    public static void main(String[] args) {
+        //Pour le test, on ne placera qu'un seul bateau
+        JoueurBataille2 j3=new JoueurBataille2("Joffrey");
+        JoueurBataille2 j4=new JoueurBataille2("Conrad");
+        JoueurBataille2 j5;
+        Scanner sc=new Scanner(System.in);
+        Bataille bataille=new Bataille(j3,j4);
+        do{     //il faut rentrer obligatoirement au moins une fois dans la boucle, car au début, la liste des bateaux de chaque joueur est vide également
+            bataille.tourSuivantTest(j3,j4 );
+        }while(!bataille.partieFinie());
+        j5=bataille.retournerGagnant();
+        bataille.afficheGagnant(j5);
+    }
+}
+
