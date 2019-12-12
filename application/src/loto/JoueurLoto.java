764f38cee38fd40992b330bc8cce1b9cc58005ec
Conrad <conradbaudelet@gmail.com>
2019-12-12 10:45:06 +0100

    -Ajout de la JavaDoc de JoueurLoto

--- application/src/loto/JoueurLoto.java
+++ application/src/loto/JoueurLoto.java
@@ -7,11 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 class JoueurLoto extends Joueur {
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -2464103437009606435L;
+    /**
+     * Grille du joueur
+     */
 	private Grille grille;
+    /**
+     * liste des chiffre coché par le Joueur
+     */
     private List<Integer> cochee;
 
     JoueurLoto(String nom) {
@@ -27,11 +31,19 @@ class JoueurLoto extends Joueur {
         return cochee;
     }
 
+    /**
+     * Initialise la grille du joueur
+     */
     void initGrille() {
         this.grille = new Grille(3,9);
         this.grille.initTab();
     }
-
+    /**
+     *
+     * @param i ijieme Case à coché
+     * @param j jIeme Case à coché
+     * @return  vrai si la case est différente de coché
+     */
     Boolean cocher(int i, int j) {
         System.out.println(grille.getMatrice()[i][j]);
         if(grille.getMatrice()[i][j] == 0) return false;
