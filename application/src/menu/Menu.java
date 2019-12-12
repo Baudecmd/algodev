77d56d7ea46092b2e8df7abab5fb519c9f23efba
Conrad <conradbaudelet@gmail.com>
2019-12-12 11:18:20 +0100

    -BugFix

--- application/src/menu/Menu.java
+++ application/src/menu/Menu.java
@@ -75,7 +75,7 @@ public class Menu extends Application {
 	}
 
 /////////////////////////////////////////
-//Ajouter les méthodes pour lancer les parties ic
+//Ajouter les mï¿½thodes pour lancer les parties ic
 	
 	public void buttonLoto() {
 		
@@ -86,14 +86,13 @@ public class Menu extends Application {
 		Stage stage2 = (Stage) this.loto.getScene().getWindow();
 		SelecGrille s= new SelecGrille();
 		s.start(stage2);
-
 	}
 
 
 	public void buttonBataille() {
 		Window w = loto.getScene().getWindow();
 		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setContentText("Jeu encore en développement");
+		alert.setContentText("Jeu encore en dï¿½veloppement");
 		alert.initOwner(w);
 		alert.show();
 
@@ -102,7 +101,7 @@ public class Menu extends Application {
 	public void buttonPoker() {
 		Window w = loto.getScene().getWindow();
 		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setContentText("Jeu encore en développement");
+		alert.setContentText("Jeu encore en dï¿½veloppement");
 		alert.initOwner(w);
 		alert.show();
 
@@ -124,8 +123,8 @@ public class Menu extends Application {
 	@FXML
 	public void handleNbJoueur(ActionEvent event) throws IOException {
 		Window w = entrerNbJoueur.getScene().getWindow();
-		// Vérif
-		System.out.println("Nombre de joueurs entré :" + nbJoueur.getText());
+		// Vï¿½rif
+		System.out.println("Nombre de joueurs entrï¿½ :" + nbJoueur.getText());
 		if (nbJoueur.getText().isEmpty()) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setContentText("Entrer un nombre de joueurs !");
@@ -163,7 +162,7 @@ public class Menu extends Application {
 			if (Menu.nbJoueurInt > 0) {
 				Joueur j = new Joueur(nomJoueur.getText());
 				Menu.nomsJoueurs.add(j);
-				creationLabel("Bonjour, " + nomJoueur.getText() + " vous êtes le joueur " + Menu.i++ + "!");
+				creationLabel("Bonjour, " + nomJoueur.getText() + " vous ï¿½tes le joueur " + Menu.i++ + "!");
 				Menu.nbJoueurInt--;
 				System.out.println("Nombre de joueurs restants:" + Menu.nbJoueurInt);
 
